9d81614bc3309d4d051137f9d0569ec3b82f2dca
Yerbolat Koptleuov <kopt.er11235@gmail.com>
2019-10-05 17:41:53 +0900

    working accel

--- app/src/main/java/com/example/bikefinder/MainActivity.java
+++ app/src/main/java/com/example/bikefinder/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.bikefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.os.Bundle;
 
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     Sensor sensor;
+    TextView text;
     SensorManager sensorManager;
     SensorEventListener sensorEventListener;
 
@@ -22,6 +27,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        text = findViewById(R.id.hi);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        text.setText("X: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
