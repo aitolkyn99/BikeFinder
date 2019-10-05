dd536f4e363f0dffa0f93d9d0b9b90c854c19cc4
Yerbolat Koptleuov <kopt.er11235@gmail.com>
2019-10-05 17:09:26 +0900

    first commitment

--- app/src/main/java/com/example/bikefinder/MainActivity.java
+++ app/src/main/java/com/example/bikefinder/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.bikefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
 import android.os.Bundle;
 
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
+    Sensor sensor;
     SensorManager sensorManager;
     SensorEventListener sensorEventListener;
 
@@ -16,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 }
